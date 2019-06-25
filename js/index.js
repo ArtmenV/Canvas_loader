2eb633aa1456c7c654c2d1711704061d6bdd81e4
Artmen_V <viliutich.artem@gmail.com>
2019-06-25 13:39:44 +0700

    funny_modal_hide

--- js/index.js
+++ js/index.js
@@ -1,20 +1,6 @@
 window.onload = function() {
-    let canvas = document.querySelector('#canvas');
-    let ctx = canvas.getContext('2d');
-
-    let clicks = [[0, 0]];
-
-    canvas.onclick = function(e) {
-        e = e || window.event;
-        clicks.push([e.offsetX, e.offsetY]);
-        
-        if(clicks.length > 2) {
-            ctx.moveTo(clicks[clicks.length-3][0], clicks[clicks.length-3][1]);
-            ctx.lineTo(e.offsetX, e.offsetY);
-        }
-        ctx.moveTo(clicks[clicks.length-2][0], clicks[clicks.length-2][1]);
-        ctx.lineTo(e.offsetX, e.offsetY);
-
-        ctx.stroke();
+    document.querySelector('.triangle').onclick = 
+    function() {
+        console.log('work');
     }
 }
