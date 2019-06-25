312a1a0d2f683eb411fba2adc1536676de0200f0
Artmen_V <viliutich.artem@gmail.com>
2019-06-25 11:03:55 +0700

    canvas

--- js/index.js
+++ js/index.js
@@ -4,16 +4,17 @@ var ctx = canvas.getContext('2d');
 var pi= Math.PI;
 var k = 2*pi/100;
 var x = 0;
-var max = 33;
+var max = 96;
 var timeout;
 
 draw();
 
 function draw() {
+    var color = getColor(x);
     ctx.clearRect(0, 0, 250, 250);
     ctx.beginPath();
         ctx.arc(125, 125, 100, 0, x * k, false);
-        ctx.strokeStyle = 'orange';
+        ctx.strokeStyle = color;
         ctx.lineWidth = 20;
         ctx.lineCap = 'round';
         ctx.stroke();
@@ -21,8 +22,8 @@ function draw() {
     x++;
 
     ctx.beginPath();
-    ctx.font = 'bold 70px Arial';
-    ctx.fillStyle = 'orange';
+    ctx.font = 'bold 55px Arial';
+    ctx.fillStyle = color;
     ctx.textAlign = 'center';
     ctx.fillText(x+'%', 125, 145);
     ctx.closePath();
@@ -35,4 +36,12 @@ function draw() {
     else {
         clearTimeout(timeout);
     }
+}
+
+function getColor(x) {
+    x = x*255/100;
+    y = 255 - x;
+    var color = 'rgb('+x+', '+y+', 0)';
+    console.log(color);
+    return color;
 }
